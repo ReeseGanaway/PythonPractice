c950fa461c046dbdcfbdf33e83c6d599322ecdc3
Reese Ganaway <reeseganaway@gmail.com>
2023-06-21 15:57:08 -0400

    very simple, incomplete madlib, will expand later maber

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+# youtuber = "Superman"
+
+# print(f"{youtuber} saved the day!\n")
+
+adjective = input("Adjective: ")
+verb = input("Verb: ")
+
+madlib = f"Fighting villains is {adjective}. That's why I {verb} evil!"
+
+print(madlib)
